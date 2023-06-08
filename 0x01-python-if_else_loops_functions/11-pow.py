fbddfff78091387e0129d32300baa105d8c293ae
Innocent_Bern <innocentbern22@gmail.com>
2023-06-08 04:56:55 +0300

    Changes task 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
-    for i in range(b):
-        a *= a
+    if b == 0:
+        return 1
+    elif b > 0:
+        for i in range(b - 1):
+            a *= a
+    elif b < 0:
+        for i in range(b * -1 - 1):
+            a = a * a
+        a = 1 / a
     return a
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -9,6 +9,4 @@ def fizzbuzz():
             print("Fizz", end=" ")
         else:
             print(i, end=" ")
-
-
-print("$")
+    print("$")
