05148936667737969cf00e0d0ae251e96dccf508
Innocent_Bern <innocentbern22@gmail.com>
2023-06-08 03:28:29 +0300

    Changes task 3 -6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(100):
     if ((i % 10) * 10) + (i // 10) > i:
         if i + 1 == 90:
-            print(f"{89}")
+            print(89)
         else:
-            print(f"{i:02d}", end=", ")
+            print("{:02d}".format(i), end=", ")
