f8d577230562785b4fb2bf404770764c89d1415c
Innocent_Bern <innocentbern22@gmail.com>
2023-06-27 19:37:13 +0300

    task-1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""class that defines a square with size attribute"""
+
+
+class Square:
+
+    """Class square with private attribute size"""
+
+    def __init__(self, size):
+        """init square with private attribute size"""
+
+        self.__size = size
