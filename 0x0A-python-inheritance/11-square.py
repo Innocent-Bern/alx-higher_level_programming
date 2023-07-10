8430ecffdd9b031d388202639c25f1f83354f06f
Innocent_Bern <innocentbern22@gmail.com>
2023-07-10 21:08:46 +0300

    task 10 && 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""class module"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """square class"""
+
+    def __init__(self, size):
+        """initializes square"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """return string representation of square"""
+
+        return f"[Square] {self.__size}/{self.__size}"
+
+    def area(self):
+        """returns area of square"""
+
+        return self.__size**2
