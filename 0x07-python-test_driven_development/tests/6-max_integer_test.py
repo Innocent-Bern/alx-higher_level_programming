29223ad1a7f7d58ea2a772023874db5aa3b078a4
Innocent_Bern <innocentbern22@gmail.com>
2023-07-20 10:15:32 +0300

    Task 5

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -17,5 +17,24 @@ class TestMaxInteger(unittest.TestCase):
         """test empty list"""
         self.assertEqual(max_integer([]), None)
 
+    def test_ints_and_floats(self):
+        """Test a list of ints and floats."""
+        ints_and_floats = [1.53, 15.5, -9, 15, 6]
+        self.assertEqual(max_integer(ints_and_floats), 15.5)
+
+    def test_string(self):
+        """Test a string."""
+        string = "Brennan"
+        self.assertEqual(max_integer(string), 'r')
+
+    def test_list_of_strings(self):
+        """Test a list of strings."""
+        strings = ["Brennan", "is", "my", "name"]
+        self.assertEqual(max_integer(strings), "name")
+
+    def test_empty_string(self):
+        """Test an empty string."""
+        self.assertEqual(max_integer(""), None)
+
 if __name__ == "__main__":
     unittest.main()
