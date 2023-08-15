9a82e50a720b00e258cdaf63f0ee6ae2d3c9b773
Innocent_Bern <innocentbern22@gmail.com>
2023-08-15 23:26:17 +0300

    till task 16

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- removes all records with a score less than 5
+DELETE FROM `second_table`
+WHERE `score` <= 5;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- list number of records with the same score
+SELECT `score`, COUNT(*) AS `number`
+from `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
