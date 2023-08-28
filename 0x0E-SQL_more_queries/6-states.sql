a11de18514dc69d5a5da46c9e00b147764b9ccf0
Innocent_Bern <innocentbern22@gmail.com>
2023-08-28 20:26:09 +0300

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,9 +2,9 @@
 -- and the table states (in the database hbtn_0d_usa
 
 CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
-CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
 	id INT NOT NULL AUTO_INCREMENT, 
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
 	);
-
