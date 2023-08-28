10180d90c4dddca83cbb3b4000ac574843d595fd
Innocent_Bern <innocentbern22@gmail.com>
2023-08-28 21:07:59 +0300

    task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,6 @@
 
 SELECT id, name
 FROM cities WHERE state_id = (
-	SELECT id 
-	FROM states WHERE name = 'Calfornia'
+	SELECT id FROM states WHERE name = 'California'
 )
-ORDER BY id;
+ORDER BY id ASC;
