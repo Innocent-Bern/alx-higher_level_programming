150b9077aa18020617c864afb4b6f5b317840ac6
Innocent_Bern <innocentbern22@gmail.com>
2023-09-12 19:12:54 +0300

    task-8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  const cpy = [...list];
+  let x = 0;
+
+  while (x < cpy.length) {
+    list[cpy.length - 1 - x] = cpy[x];
+    x += 1;
+  }
+  return list;
 };
