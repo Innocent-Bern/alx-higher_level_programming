4f30543f6066be1736d6244805ef7d1ab7ea2517
Innocent_Bern <innocentbern22@gmail.com>
2023-09-30 01:48:15 +0300

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,5 +11,5 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    response = requests.post(argv[1], argv[2])
+    response = requests.post(argv[1], {"email": argv[2]})
     print(response.text)
