d26a680b04ae95d332c113bf607c4951e62ffa01
Innocent_Bern <innocentbern22@gmail.com>
2023-09-30 19:30:55 +0300

    task-4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -21,5 +21,6 @@ if __name__ == "__main__":
             "SELECT cities.id, cities.name, states.name \
             FROM cities JOIN states ON cities.states_id = states.id \
             ORDER BY states.id ASC;")
+
     for row in cur.fetchall():
         print(row)
