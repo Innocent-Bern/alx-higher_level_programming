2bb1874d53be3971cc5aa96e3fb2c004e8d1963e
Innocent_Bern <innocentbern22@gmail.com>
2023-10-25 17:36:34 +0300

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = `${process.argv[2]}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(`${process.argv[3]}`, body, 'utf8', err => {
+      if (err) console.error(err);
+    });
+  }
+});
