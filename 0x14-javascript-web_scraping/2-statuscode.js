ac59684c7806504e4cccf90d92aef0471e952c29
Innocent_Bern <innocentbern22@gmail.com>
2023-10-25 14:56:50 +0300

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@
 const request = require('request');
 
 request(`${process.argv[2]}`, (error, response, body) => {
-	if (error) console.error(error);
-	console.log(response.statusCode);
-})
+  if (error) console.error(error);
+  console.log(`code: ${response.statusCode}`);
+});
